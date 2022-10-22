40d7d7be09e475249c48660c40dc8b02c8bf0091
devtimnbr <timnbr99@gmail.com>
2022-10-22 22:22:40 +0200

    add first responsive changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	safelist: ['md:flex-row-reverse'],
 	theme: {
-		extend: {},
+		extend: {
+			flex: {
+				"2": "2",
+				"1/3": "0 0 33.333%"
+			}
+		},
 	},
+	
 	plugins: [],
 }
