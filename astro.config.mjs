26af70a87d849e9aa75218e297adde758364dcb8
devtimnbr <timnbr99@gmail.com>
2022-10-24 23:24:56 +0200

    fix tailwind base style

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind()],
   vite: {
     ssr: {
       external: ["svgo"]
